74c822b02389101fab106e5d10fb3cc3ace96a99
Jason Louie <jason.louie29@myhunter.cuny.edu>
2025-10-22 15:31:55 -0400

    fixed error with prop

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -5,12 +5,12 @@ export default function EmployeeList() {
         <div className="employee-list">
             <EmployeeListItem name="Tommy Pickles" src="/images/tommypickles.jpg" position="Rugrats Protagonist" />
             <EmployeeListItem name="Chuckie Finster" src="/images/chuckiefinster.png" position="Rugrats Supporting Character"/>
-            <EmployeeListItem name="John Doe" title="Webmaster"/>
-            <EmployeeListItem name="Jane Smith" title="Full Stack Developer"/>
-            <EmployeeListItem name="Mike Jones" title="Front-End Developer"/>
-            <EmployeeListItem name="Mary Anderson" title="Marketing Manager"/>
-            <EmployeeListItem name="Susan Lee" title="Social Media Manager"/>
-            <EmployeeListItem name="James Johnson" title="Accountant"/>
+            <EmployeeListItem name="John Doe" position="Webmaster"/>
+            <EmployeeListItem name="Jane Smith" position="Full Stack Developer"/>
+            <EmployeeListItem name="Mike Jones" position="Front-End Developer"/>
+            <EmployeeListItem name="Mary Anderson" position="Marketing Manager"/>
+            <EmployeeListItem name="Susan Lee" position="Social Media Manager"/>
+            <EmployeeListItem name="James Johnson" position="Accountant"/>
         </div>
     );
 }
